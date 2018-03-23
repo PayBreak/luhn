bb46aea02249da7b2422e753ebf6648a7db3a268
Simon Dawson <sped2773@yahoo.com>
2018-03-23 14:56:46 +0000

    Apply fixes from StyleCI

--- src/Luhn.php
+++ src/Luhn.php
@@ -77,8 +77,7 @@ class Luhn
 
         $sum = 0;
 
-        foreach($data as $k => $v) {
-
+        foreach ($data as $k => $v) {
             $tmp = $v + $v * (int)(($k % 2) xor !$check);
 
             if ($tmp > 9) {
